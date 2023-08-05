e573e9b2d6ba4b0c3e0f98c70fa8e5565ec55de7
JawadMM <Almuttawa.j@hotmail.com>
2023-08-05 13:57:14 +0300

    register form added

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const app = express();
 const User = require("./models/user");
+
+mongoose.connect("mongodb://127.0.0.1:27017/authDemo");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("HomePage");
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const newUser = new User({ username, password: hash });
+  await newUser.save();
+  res.redirect("/");
 });
 
 app.get("/secret", (req, res) => {
